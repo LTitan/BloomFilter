f3c7074edaef84f58bee9e13d6dd72927cc33e41
LTitan <m15169917023@163.com>
2020-01-31 16:41:42 +0800

    cpu useage

--- internal/bloomfilter/grpc.go
+++ internal/bloomfilter/grpc.go
@@ -19,6 +19,8 @@ const (
 func RunClient() {
 	preCPU, _ := cpu.Get()
 	tm := time.NewTicker(time.Minute * 3)
+	cpuTicker := time.NewTicker(time.Second * 5)
+	var sumCPUUsage, cnt float32
 	for {
 		select {
 		case <-tm.C:
@@ -27,17 +29,22 @@ func RunClient() {
 				panic(err)
 			}
 			c := rpc.NewGreeterClient(conn)
-			currCPU, _ := cpu.Get()
 			memoryInfo, _ := memory.Get()
 			ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 			_, _ = c.HeartBeat(ctx, &rpc.MachineInfo{
-				Cpu:         uint32(currCPU.CPUCount),
-				Memory:      memoryInfo.Total / 1048576,
-				CpuUsage:    float32(currCPU.System-preCPU.System) / float32(currCPU.Total-preCPU.Total) * 100,
-				MemoryUsage: float32((memoryInfo.Used/1048576)) / float32((memoryInfo.Total/1048576)) * 100,
+				Cpu:         uint32(preCPU.CPUCount),
+				Memory:      memoryInfo.Total / memBase,
+				CpuUsage:    sumCPUUsage / cnt,
+				MemoryUsage: float32((memoryInfo.Used / memBase)) / float32((memoryInfo.Total / memBase)) * 100,
 			})
 			cancel()
+			cnt = 0
+			sumCPUUsage = 0
+		case <-cpuTicker.C:
+			currCPU, _ := cpu.Get()
+			sumCPUUsage += (float32(currCPU.System-preCPU.System) / float32(currCPU.Total-preCPU.Total) * 100)
 			preCPU = currCPU
+			cnt++
 		}
 	}
 }
